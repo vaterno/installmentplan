687327ab64f9ebc4ad7472d7d6cc032ec67a647a
Ady Pryce <ady.pryce@kiv.dev>
2023-05-14 17:33:29 +0300

    1112

--- metadata.php
+++ metadata.php
@@ -3,21 +3,15 @@
 $sMetadataVersion = '1.0';
 
 $aModule = [
-    'id' => 'installmentplan',
-    'title' => 'Installment plan',
-    'description' => [
-        'en' => 'Installment plan',
-        'de' => 'Installment plan',
-    ],
-    'thumbnail' => 'logo.png',
-    'version' => '1.0.0',
-    'author' => 'Vaterno',
-    'url' => 'http://www.oxid-esales.com',
-    'email' => 'info@oxid-esales.com',
-    'extend' => [],
-    'controllers' => [],
-    'templates' => [],
-    'events' => [],
-    'blocks' => [],
-    'settings' => [],
+    'id'            => 'installmentplan',
+    'title'         => 'Installment plan',
+    'description'   => 'Installment plan',
+    'thumbnail'     => 'logo.png',
+    'version'       => $sMetadataVersion,
+    'author'        => 'Vaterno',
+    'blocks' => [
+        'template' => 'layout/base.tpl',
+        'block' => 'installmentplan_banner',
+        'file' => 'src/views/blocks/installmentplan_banner.tpl'
+    ]
 ];
