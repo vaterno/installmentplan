960065ebe3494f8a3325862e75ee75d1759655ba
Ady Pryce <ady.pryce@kiv.dev>
2023-05-14 17:38:28 +0300

    2e24

--- metadata.php
+++ metadata.php
@@ -5,13 +5,18 @@ $sMetadataVersion = '1.0';
 $aModule = [
     'id'            => 'installmentplan',
     'title'         => 'Installment plan',
-    'description'   => 'Installment plan',
+    'description'   => [
+        'en' => 'Installment plan',
+        'de' => 'Installment plan',
+    ],
     'thumbnail'     => 'logo.png',
     'version'       => $sMetadataVersion,
     'author'        => 'Vaterno',
     'blocks' => [
-        'template' => 'layout/base.tpl',
-        'block' => 'installmentplan_banner',
-        'file' => 'src/views/blocks/installmentplan_banner.tpl'
+        [
+            'template' => 'layout/base.tpl',
+            'block' => 'installmentplan_banner',
+            'file' => 'src/views/blocks/installmentplan_banner.tpl'
+        ]
     ]
 ];
