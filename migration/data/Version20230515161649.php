c88e5f7fec6440688801b5f5c11402c6c5c88179
Ady Pryce <ady.pryce@kiv.dev>
2023-05-15 19:22:27 +0300

    Update

--- migration/data/Version20230515161649.php
+++ migration/data/Version20230515161649.php
@@ -7,15 +7,18 @@ namespace Vaterno\Installmentplan\Migrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20210126151019 extends AbstractMigration
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230515161649 extends AbstractMigration
 {
-    public function up(Schema $schema): void
+    public function up(Schema $schema) : void
     {
         $this->addSql('ALTER TABLE `oxarticles` ADD COLUMN `OXNUMBERINSTALLMENTMONTHS` DOUBLE(10, 2)');
         $this->addSql('ALTER TABLE `oxarticles` ADD COLUMN `OXPREPAYMENTAMOUNT` DOUBLE(10, 2)');
     }
 
-    public function down(Schema $schema): void
+    public function down(Schema $schema) : void
     {
         $this->addSql('ALTER TABLE `oxarticles` DROP COLUMN `OXNUMBERINSTALLMENTMONTHS`');
         $this->addSql('ALTER TABLE `oxarticles` DROP COLUMN `OXPREPAYMENTAMOUNT`');
